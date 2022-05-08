c20c54c147b96be77f8be32ec774164a2b4442f1
Blackswan99 <37673853+Blackswan99@users.noreply.github.com>
2022-05-08 20:54:34 +0200

    Initial

--- 03/solution1.ps1
+++ 03/solution1.ps1
@@ -1,3 +1,3 @@
-$payload = Get-Content -path "C:\Users\Zwette\Documents\GitHub\Advent-of-Code-2021\02\input.txt"
+$payload = Get-Content -Path "input.txt"
 
 Write-Host "Final position: h=$horizontal v=$vertical ==> Multiply: $($horizontal*$vertical)" 
