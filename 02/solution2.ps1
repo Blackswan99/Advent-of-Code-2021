9074ec61ef6752efa18541a4f0ee81f5302c4d7d
Blackswan99 <37673853+Blackswan99@users.noreply.github.com>
2022-05-08 20:48:06 +0200

    Final solution

---
+++ 02/solution2.ps1
@@ -0,0 +1,15 @@
+$payload = get-content -path "C:\Users\Zwette\Documents\GitHub\Advent-of-Code-2021\02\input.txt"
+$horizontal = 0
+$vertical = 0
+$aim = 0
+for ($i = 0; $i -lt ($payload.count); $i++){
+    switch ($payload[$i].Split(" ")[0]){
+        "forward" { 
+            $horizontal += [int]$($payload[$i].Split(" ")[1]) 
+            $vertical += $aim * [int]$($payload[$i].Split(" ")[1])
+        }
+        "down" { $aim += [int]$($payload[$i].Split(" ")[1]) }
+        "up"{ $aim -= [int]$($payload[$i].Split(" ")[1]) }
+    }
+}
+Write-Host "Final position: h=$horizontal v=$vertical ==> Multiply: $($horizontal*$vertical)" 
