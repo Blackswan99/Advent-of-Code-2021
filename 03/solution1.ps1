bdc1183bbf401859c108932a841fd3f753bca23a
Blackswan99 <37673853+Blackswan99@users.noreply.github.com>
2022-05-12 20:25:54 +0200

    New day

--- 03/solution1.ps1
+++ 03/solution1.ps1
@@ -1,3 +1,9 @@
 $payload = Get-Content -Path "input.txt"
 
-Write-Host "Final position: h=$horizontal v=$vertical ==> Multiply: $($horizontal*$vertical)" 
+for ($i = 1; $i -lt $($payload.Count + 1); $i++){
+    for ($j = 1; $j -lt $($payload[$i].Length+1); $j++){
+        
+        Write-Host "$($payload[$i][$j])" -NoNewline
+    }
+    Write-Host
+}
